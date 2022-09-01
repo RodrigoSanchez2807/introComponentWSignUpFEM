b2cb7ce6d0425a9400987cdfbe36a6b489a0a871
RodrigoSanchez2807 <rodalbertlopez@gmail.com>
2022-08-31 19:32:23 -0500

    validation

--- js/index.js
+++ js/index.js
@@ -1,42 +1,43 @@
 // Variables
 const form = document.querySelector('#form');
-const btn = document.querySelector('#btn-trial');
+// const btn = document.querySelector('#btn-trial');
 
 const firstName = document.querySelector('#first-name');
-const LastName = document.querySelector('#last-name');
+const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
+
+eventListeners();
 //Event listeners
 function eventListeners() {
 
-    // form.addEventListener('DOMContentLoaded', validate);
-
-    form.addEventListener('submit', validate);
+    firstName.addEventListener('blur', validation);
+    lastName.addEventListener('blur', validation);
+    email.addEventListener('blur', validation);
+    password.addEventListener('blur', validation);
     
-
-    
-
 }
 
+//Functions
 
+// function starting (){
 
+// }
 
-function validate(e){
+function validation(e){
     e.preventDefault();
 
-    console.log('diste click en el botón');
+    if(e.target.value.length > 0){
+        //Hide icon
+        e.target.classList.remove('error'); 
+    } else{
+        //Show icon
+        e.target.classList.add('error'); 
 
-
-
-    
-    
-    
-    
-    
-    
-    
-    
+        showMessage();
+    }
+  
     // para enviar el formulario
 
     // if(er.test(email.value) !== '' && asunto.value !== '' && mensaje.value !== ''){
@@ -44,7 +45,12 @@ function validate(e){
     //     btnEnviar.disabled = false;
     //     btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
     // }
+}
 
-
-    
+function showMessage() {
+    //Show message
+    const warning = document.createElement('p'); 
+    warning.textContent = 'probando';
+    warning.classList.add('message');    
+    lastName.appendChild(warning);
 }
